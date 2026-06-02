24a15cee190f55f5c63d363465ad2884ab3df1e4
alumno <alumno@Instituto.local>
2026-06-02 09:50:35 -0300

    holoa
    ghfg

---
+++ Trabajo.Practico5/app.js
@@ -0,0 +1,10 @@
+
+//Declarar variable
+let numero1
+//Asignar valor
+numero1 = 10
+//Declarar y asignar al mismo tiempo
+let numero2= 30
+numero1 = 5
+//Mostrar la información
+console.log(numero1)
