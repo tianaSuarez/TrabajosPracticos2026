162420fa4eed666aabef0f1e70beea257235c638
alumno <52178120@ivt.edu.ar>
2026-06-04 10:39:04 -0300

    4 de Junio
    holaholamanola

--- Trabajo.Practico5/app.js
+++ Trabajo.Practico5/app.js
@@ -4,7 +4,12 @@ let numero1
 //Asignar valor
 numero1 = 10
 //Declarar y asignar al mismo tiempo
-let numero2= 30
+let numero2= 10 + numero1
 numero1 = 5
 //Mostrar la información
-console.log(numero1)
+console.log("Nos vemos en diciembre")
+
+let suma
+let división
+let multiplicación
+let resta
